075bac7fe1dbc116618294434f56786a072a985b
urvi2408 <bhavsarurvi143@gmail.com>
2025-01-06 15:50:29 +0530

    Added simple grid in app js

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,29 @@
-import './App.css'
-import GridLayout from "react-grid-layout";
-// import CustomGridLayout from './common/customGridLayout';
+import { Card } from 'antd';
+import './App.css';
+import CustomGridLayout from './common/customGridLayout';
 
 const App = () => {
-
-  const layout = [
-    { i: "a", x: 0, y: 0, w: 1, h: 2, static: true },
-    { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
-    { i: "c", x: 4, y: 0, w: 1, h: 2 }
-  ];
-
   return (
     <>
-
-      <GridLayout
-        className="layout"
-        layout={layout}
+      <CustomGridLayout
+        className="custom-grid"
+        layout={[
+          { i: "a", x: 0, y: 0, w: 1, h: 2 },
+          { i: "b", x: 1, y: 0, w: 3, h: 2 },
+        ]}
         cols={12}
         rowHeight={30}
-        width={1200}
+        width={1000}
       >
-        <div key="a">a</div>
-        <div key="b">b</div>
-        <div key="c">c</div>
-      </GridLayout>
+        <div key="a">Content A
+          <Card title="Card title" bordered={false} style={{ width: 300 }}>
+          </Card>
+        </div>
+        <div key="b">Content B</div>
+        <div key="c">Content C</div>
+      </CustomGridLayout>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
