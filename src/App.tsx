60b2c01f1054c83e5a3f9e642b6991ca1706c4c4
urvi2408 <bhavsarurvi143@gmail.com>
2025-01-06 17:50:19 +0530

    [ADDED]resizable grid layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { Card } from 'antd';
-import './App.css';
+// import './App.css';
 import CustomGridLayout from './common/customGridLayout';
+import "/node_modules/react-grid-layout/css/styles.css";
+import "/node_modules/react-resizable/css/styles.css"
+ 
+
 
 const App = () => {
   return (
