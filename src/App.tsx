b1cd51fcc2ad494ea249f7bea6a18107a103c0b2
TejashTntra <tejash.jadhav@tntra.io>
2025-01-07 12:29:13 +0530

    Added Components and changed the structure

--- src/App.tsx
+++ src/App.tsx
@@ -3,36 +3,14 @@ import "/node_modules/react-grid-layout/css/styles.css";
 import "/node_modules/react-resizable/css/styles.css";
 import CalenderWidget from "./components/calender";
 import WeatherWidget from "./components/weather";
-import styled from "styled-components";
 import GoogleMapsWidget from "./components/googleMaps";
 import NewsWidget from "./components/news";
 import StockMarketWidget from "./components/stockMarket";
 import QuotesWidget from "./components/quotes";
 
+import "./App.css"; 
+
 const App = () => {
-  const GridItemWrapper = styled.div`
-    background: #f5f5f5;
-    height: 100%; /* Ensures it takes the full height of the grid item */
-    display: flex; /* Ensures child content is properly aligned */
-    justify-content: center;
-    align-items: center;
-    border-radius: 8px; /* Optional: Adds rounded corners */
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* Optional: Adds a shadow for visual appeal */
-  `;
-
-  const GridItemContent = styled.div`
-    padding: 8px;
-    width: 100%;
-    height: 100%; /* Matches the parent wrapper's height */
-    display: flex; /* Ensures child content is properly aligned */
-    justify-content: center;
-    align-items: center;
-  `;
-
-  const Root = styled.div`
-    padding: 16px;
-  `;
-
   const widgetConfigs = [
     { key: "calenderWidget", component: <CalenderWidget />, x: 0, y: 0, w: 2, h: 2 },
     { key: "weatherWidget", component: <WeatherWidget />, x: 2, y: 0, w: 2, h: 2 },
@@ -43,21 +21,21 @@ const App = () => {
   ];
   
   return (
-    <Root>
-    <CustomGridLayout
-      className="layout"
-      layout={widgetConfigs.map(({ key, x, y, w, h }) => ({ i: key, x, y, w, h }))}
-      cols={12}
-      rowHeight={30}
-      width={1300}
-    >
-      {widgetConfigs.map(({ key, component }) => (
-        <GridItemWrapper key={key}>
-          <GridItemContent>{component}</GridItemContent>
-        </GridItemWrapper>
-      ))}
-    </CustomGridLayout>
-  </Root>
+    <div className="root">
+      <CustomGridLayout
+        className="layout"
+        layout={widgetConfigs.map(({ key, x, y, w, h }) => ({ i: key, x, y, w, h }))}
+        cols={12}
+        rowHeight={30}
+        width={1300}
+      >
+        {widgetConfigs.map(({ key, component }) => (
+          <div className="grid-item-wrapper" key={key}>
+            <div className="grid-item-content">{component}</div>
+          </div>
+        ))}
+      </CustomGridLayout>
+    </div>
   );
 };
 
