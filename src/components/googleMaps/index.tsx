4eadb22734a4c80b5d3f000bac59493f3284ee40
urvi2408 <bhavsarurvi143@gmail.com>
2025-01-08 14:09:40 +0530

    [ADDED]simple map component.

--- src/components/googleMaps/index.tsx
+++ src/components/googleMaps/index.tsx
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+
+type HoverInfo = {
+  country: string | null;
+  position: { x: number; y: number };
+};
 
 const GoogleMapsWidget: React.FC = () => {
-  return "Google Maps Widget";
+  const geoUrl = "https://unpkg.com/world-atlas@2/countries-50m.json";
+  const [hoverInfo, setHoverInfo] = useState<HoverInfo | null>(null);
+
+  const handleMouseEnter = (geo: any, event: React.MouseEvent) => {
+    const countryName = geo.properties.name;
+
+    setHoverInfo({
+      country: countryName,
+      position: { x: event.pageX, y: event.pageY },
+    });
+  };
+
+  const handleMouseLeave = () => {
+    setHoverInfo(null);
+  };
+
+  return (
+    <div>
+      <ComposableMap>
+        <Geographies geography={geoUrl}>
+          {({ geographies }) =>
+            geographies.map((geo) => (
+              <Geography
+                key={geo.rsmKey}
+                geography={geo}
+                onMouseEnter={(event) => handleMouseEnter(geo, event)}
+                onMouseLeave={handleMouseLeave}
+                style={{
+                  default: { fill: "#D6D6DA", outline: "none" },
+                  hover: { fill: "#F53", outline: "none" },
+                  pressed: { fill: "#E42", outline: "none" },
+                }}
+              />
+            ))
+          }
+        </Geographies>
+      </ComposableMap>
+
+      {hoverInfo && (
+        <div className="tool-tip">
+          <div>{hoverInfo.country}</div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default GoogleMapsWidget;
