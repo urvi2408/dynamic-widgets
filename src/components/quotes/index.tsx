1a16f287605de870aee98d80077a0379c1ed775a
UrviBhavsar <urvi.bhavsar@tntra.io>
2025-01-08 18:38:35 +0530

    add card in stock market and quotes widgets

--- src/components/quotes/index.tsx
+++ src/components/quotes/index.tsx
@@ -1,8 +1,20 @@
-import { Image } from "antd";
+import { Card, Image } from "antd";
 import quoteImg from "../../assets/images.jpeg";
 
 const QuotesWidget = () => {
-  return <Image src={quoteImg} alt="Quote" />;
+  return (
+    <Card
+      title="Quotes Widget"
+      bordered
+      style={{
+        width: 300,
+        borderRadius: "6px",
+        boxShadow: "0px 0px 24px 0px rgba(0,0,0,0.1)",
+      }}
+    >
+      <Image src={quoteImg} alt="Quote" />
+    </Card>
+  );
 };
 
 export default QuotesWidget;
